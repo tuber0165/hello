8253e4a860bdd4cbf3a42038e1f4968f9b62c626
tuber0165 <1030984200@qq.com>
2024-09-23 16:06:49 +0800

    sum of a+b

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    
+    cout << "请输入a的值" << endl;  
+    cin >> a; 
+    cout << "请输入b的值" << endl;
+    cin >> b;
+    c = a + b;
+    cout << "a+b的和为" << c << endl;
+    return 0;
+
+    
+    
+}
